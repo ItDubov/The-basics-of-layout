13ce96e00f80900c9c9c6818ab589d937223e8c4
Виктор <viktor.dubov.2007@gmail.com>
2025-02-26 14:02:24 +0300

    Решение ошибок

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import HomePageView, ContactPageView, ProductDetailView
 
 urlpatterns = [
@@ -6,3 +8,7 @@ urlpatterns = [
     path('contacts/', ContactPageView.as_view(), name='contacts'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
+
+# Подключаем обработку медиафайлов
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
